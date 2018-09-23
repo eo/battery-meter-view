9f74f0cf45230e77a3e4f4ed49a315577f194da0
Erdem Orman <ormanerdem@gmail.com>
2018-09-23 18:50:22 +0300

    Color edit module is renamed and updated

---
+++ colorinput/src/main/java/eo/view/colorinput/ColorInputView.kt
@@ -0,0 +1,139 @@
+package eo.view.colorinput
+
+import android.content.Context
+import android.graphics.Color
+import android.text.Editable
+import android.text.TextWatcher
+import android.util.AttributeSet
+import android.view.View
+import android.view.inputmethod.InputMethodManager
+import android.widget.SeekBar
+import androidx.constraintlayout.widget.ConstraintLayout
+import androidx.core.content.ContextCompat
+import kotlinx.android.synthetic.main.view_color_input.view.*
+
+
+class ColorInputView @JvmOverloads constructor(
+    context: Context,
+    attrs: AttributeSet? = null,
+    defStyleAttr: Int = 0
+) : ConstraintLayout(context, attrs, defStyleAttr), SeekBar.OnSeekBarChangeListener, TextWatcher,
+    View.OnFocusChangeListener {
+
+    companion object {
+        private const val DEFAULT_COLOR = Color.BLACK
+    }
+
+    var color: Int
+        get() = Color.argb(alpha, red, green, blue)
+        set(value) {
+            alpha = Color.alpha(value)
+            red = Color.red(value)
+            green = Color.green(value)
+            blue = Color.blue(value)
+        }
+
+    private var alpha
+        get() = alphaSeekBar.progress
+        set(value) {
+            alphaSeekBar.progress = value
+            alphaValueView.text = value.toString()
+        }
+
+    private var red
+        get() = redSeekBar.progress
+        set(value) {
+            redSeekBar.progress = value
+            redValueView.text = value.toString()
+        }
+
+    private var green
+        get() = greenSeekBar.progress
+        set(value) {
+            greenSeekBar.progress = value
+            greenValueView.text = value.toString()
+        }
+
+    private var blue
+        get() = blueSeekBar.progress
+        set(value) {
+            blueSeekBar.progress = value
+            blueValueView.text = value.toString()
+        }
+
+    init {
+        inflate(context, R.layout.view_color_input, this)
+
+        hexEditText.filters = arrayOf(HexColorInputFilter())
+        setupEventHandlers()
+        color = DEFAULT_COLOR
+        updateHexInput()
+    }
+
+    private fun setupEventHandlers() {
+        hexEditText.addTextChangedListener(this)
+        hexEditText.onFocusChangeListener = this
+
+        alphaSeekBar.setOnSeekBarChangeListener(this)
+        redSeekBar.setOnSeekBarChangeListener(this)
+        greenSeekBar.setOnSeekBarChangeListener(this)
+        blueSeekBar.setOnSeekBarChangeListener(this)
+    }
+
+    private fun updateHexInput() {
+        hexEditText.removeTextChangedListener(this)
+        hexEditText.setText(context.getString(R.string.color_input_hex_value, color))
+        hexEditText.addTextChangedListener(this)
+    }
+
+    override fun onProgressChanged(seekBar: SeekBar?, progress: Int, fromUser: Boolean) {
+        if (!fromUser) return
+
+        when (seekBar) {
+            alphaSeekBar -> alpha = progress
+            redSeekBar -> red = progress
+            greenSeekBar -> green = progress
+            blueSeekBar -> blue = progress
+        }
+
+        hexEditText.clearFocus()
+        updateHexInput()
+    }
+
+    override fun onTextChanged(s: CharSequence, start: Int, before: Int, count: Int) {
+        val colorInt = if (s.isEmpty()) {
+            0
+        } else {
+            val colorHex = s.padEnd(HexColorInputFilter.MAX_LENGTH, '0').toString()
+            Color.parseColor(colorHex)
+        }
+
+        color = colorInt
+    }
+
+    override fun onFocusChange(view: View, hasFocus: Boolean) {
+        if (view == hexEditText && !hasFocus) {
+            updateHexInput()
+
+            // hide keyboard
+            ContextCompat.getSystemService(context, InputMethodManager::class.java)
+                ?.hideSoftInputFromWindow(hexEditText.windowToken, 0)
+        }
+    }
+
+    override fun onStartTrackingTouch(seekBar: SeekBar?) {
+        // no-op
+    }
+
+    override fun onStopTrackingTouch(seekBar: SeekBar?) {
+        // no-op
+    }
+
+    override fun afterTextChanged(s: Editable?) {
+        // no-op
+    }
+
+    override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+        // no-op
+    }
+}
---
+++ colorinput/src/main/java/eo/view/colorinput/HexColorInputFilter.kt
@@ -0,0 +1,58 @@
+package eo.view.colorinput
+
+import android.text.InputFilter
+import android.text.Spanned
+
+class HexColorInputFilter : InputFilter {
+
+    companion object {
+        private const val PREFIX = '#'
+        const val MAX_LENGTH = 9 // # + 8 hex characters
+    }
+
+    override fun filter(
+        source: CharSequence,
+        start: Int,
+        end: Int,
+        dest: Spanned,
+        dstart: Int,
+        dend: Int
+    ): CharSequence? {
+
+        val keep = MAX_LENGTH - (dest.length - (dend - dstart))
+        if (keep <= 0 || (dstart == 0 && dend == 0 && dest.startsWith(PREFIX))) {
+            // do not exceed max length and do not allow input in front of prefix
+            return ""
+        } else if (start == end) {
+            // allow deletion except prefix
+            return if (dstart == 0 && dend >= 1) {
+                PREFIX.toString()
+            } else {
+                null
+            }
+        }
+
+        val sourceSubSequence = source.subSequence(start, end)
+        val shouldAddPrefix = (dstart == 0 && !sourceSubSequence.startsWith(PREFIX))
+
+        var result = sourceSubSequence
+            .filterIndexed { index, c ->
+                // allow first character to be #
+                (dstart == 0 && index == 0 && c == PREFIX) || isHexCharacter(c)
+            }.toString()
+
+        if (shouldAddPrefix) {
+            result = PREFIX + result
+        }
+
+        if (result.length > keep) {
+            result = result.substring(0, keep)
+        }
+
+        return result.toUpperCase()
+    }
+
+    private fun isHexCharacter(ch: Char): Boolean {
+        return ch in ('0'..'9') || ch.toUpperCase() in ('A'..'F')
+    }
+}
